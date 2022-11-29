f823ce14f7f2dcb5c695a064ad4182d58ab88b7f
Pedrina Celia Brasil <2131716@ifrn.local>
2022-11-29 16:31:35 -0300

    filtro por categoria

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('produtos', views.ProdutoListView.as_view(), name='produtos'),
+    path('produtos/<int:tipo_id>/', views.ProdutoPorTipoListView.as_view(), name='produtos'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -5,14 +5,8 @@ from django.utils import timezone
 from django.views.generic.list import ListView
 
 def index(request):
-
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    context = {
-        
-    }
-
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html', {'tipos': Tipo.objects.all()})
 
 
 class ProdutoListView(ListView):
@@ -22,4 +16,19 @@ class ProdutoListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
+        context['tipos'] = Tipo.objects.all()
+        return context
+
+class ProdutoPorTipoListView(ListView):
+    model = Produto
+    template_name = 'catalogo/produto_list.html'
+
+    def get_queryset(self):
+        return Produto.objects.filter(tipo__id=self.kwargs['tipo_id'])
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        context['tipos'] = Tipo.objects.all()
+        context['tipo_sel'] = Tipo.objects.get(pk=self.kwargs['tipo_id'])
         return context
